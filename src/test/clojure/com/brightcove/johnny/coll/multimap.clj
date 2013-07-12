34c9187f848991dfcb047e44196a5ebb21cfa50c
Tim McCormack <tmccormack@brightcove.com>
2013-07-12 13:03:19 -0400

    Finish testing PMM.

--- src/test/clojure/com/brightcove/johnny/coll/multimap.clj
+++ src/test/clojure/com/brightcove/johnny/coll/multimap.clj
@@ -38,27 +38,45 @@
 (deftest many-roads
   (testing "Several manipulations that should all produce the same result"
     (let [goal {"foo" ["bar" "bar" "baz"] "quux" ["grovel" nil]}
-          paths {:consing (-> (PMM.)
-                              (.cons "foo" "bar")
-                              (.cons "quux" "grovel")
-                              (.cons "foo" "bar")
-                              (.cons "foo" "baz")
-                              (.cons "quux" nil))
-                 :assoc-per-key (-> (PMM.)
-                                    (.assoc "quux" ["grovel" nil])
-                                    (.assoc "foo" ["bar" "bar" "baz"]))
-                 :cons-entries (.consAll (PMM.)
-                                         ^java.util.Iterable
-                                         (for [[k vs] goal
-                                               v vs]
-                                           (first {k v})))
-                 :cons-kvs (reduce (fn [m [k vs]] (.consAll m k vs))
-                                   (PMM.)
-                                   goal)
-                 :cons-mm (let [mm (ArrayListMultimap/create)]
-                            (doseq [[k vs] goal]
-                              (.putAll mm k vs))
-                            (.consAll (PMM.) mm))}
+          consing #(-> %
+                       (.cons "foo" "bar")
+                       (.cons "quux" "grovel")
+                       (.cons "foo" "bar")
+                       (.cons "foo" "baz")
+                       (.cons "quux" nil))
+          assoc-per-key #(-> %
+                             (.assoc "quux" ["grovel" nil])
+                             (.assoc "foo" ["bar" "bar" "baz"]))
+          cons-entries #(.consAll %
+                                  ^java.util.Iterable
+                                  (for [[k vs] goal
+                                        v vs]
+                                    (first {k v})))
+          cons-kvs #(reduce (fn [m [k vs]] (.consAll m k vs)) % goal)
+          cons-mm #(let [mm (ArrayListMultimap/create)]
+                     (doseq [[k vs] goal]
+                       (.putAll mm k vs))
+                     (.consAll % mm))
+          paths {:consing (consing (PMM.))
+                 :assoc-per-key (assoc-per-key (PMM.))
+                 :cons-entries (cons-entries (PMM.))
+                 :cons-kvs (cons-kvs (PMM.))
+                 :cons-mm (cons-mm (PMM.))
+                 :drop-and-add-after (-> (consing (PMM.))
+                                         (.dropLast "quux" "grovel")
+                                         (.cons "quux" "grovel")
+                                         (.dropAll "quux" nil)
+                                         (.cons "quux" nil))
+                 :tmp-spurious (-> (cons-entries (PMM.))
+                                   (.cons "1" "2")
+                                   (.dissoc "1"))
+                 :empty (-> (.cons (PMM.) "3" "4")
+                            (.empty)
+                            cons-mm)
+                 :drop-val-sandwich (-> (PMM.)
+                                        (.cons "quux" "spurious")
+                                        assoc-per-key
+                                        (.dropLast "quux" "spurious"))}
           checkers {:asmap #(is (= (into {} (for [[k vs] (.asMap %)]
                                               [k (vec vs)]))
                                    goal))
@@ -76,7 +94,13 @@
                     :contains-goal-key #(doseq [gk (keys goal)]
                                           (is (.containsKey % gk)))
                     :contains-goal-val #(doseq [gv (apply concat (vals goal))]
-                                          (is (.containsValue % gv)))}]
+                                          (is (.containsValue % gv)))
+                    :entries #(doseq [e (.entries %)]
+                                (is (.contains (goal (key e)) (val e))))
+                    :get #(doseq [[k vs] goal]
+                            (is (= (.get % k) vs)))
+                    :no-empty-keys #(doseq [k (.keySet %)]
+                                      (is (not-empty (.get % k))))}]
       (doseq [[path val] paths]
         (testing (str "path=" (name path))
           (doseq [[ch-n ch-v] checkers]
