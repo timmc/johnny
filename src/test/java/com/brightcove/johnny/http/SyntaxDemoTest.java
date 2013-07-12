4c2fa15fb4f30be48a8d2a04722b0cd1991b0994
Tim McCormack <tmccormack@brightcove.com>
2013-07-12 15:21:38 -0400

    Visually cleaner test

--- src/test/java/com/brightcove/johnny/http/SyntaxDemoTest.java
+++ src/test/java/com/brightcove/johnny/http/SyntaxDemoTest.java
@@ -9,15 +9,25 @@ import org.junit.Test;
 /** Demonstrate basic usage. */
 public class SyntaxDemoTest {
 
-    /* NOTICE: All the real tests are in src/main/clojure -- this is just a demo. */
+    /*
+     * NOTICE: All the real tests are in src/main/clojure -- this is just
+     * a demo.
+     */
+
+    /*
+     * Also note that these tests are fragile to hashing order -- they'd need
+     * to be rewritten for anything beyond a demo.
+     */
 
     /**
      * Demonstrate setter workflow.
      */
     @Test
     public void testWorkflow() throws MalformedURLException {
-        Url orig = Urls.parse("http://brightcove.com/login?email=jrh@example.net");
-        Url result = orig.withPort(8443).withProtocol("https").querySetKey("unicode", "☃");
+        Url result = Urls.parse("http://brightcove.com/login?email=jrh@example.net")
+                         .withPort(8443)
+                         .withProtocol("https")
+                         .querySetKey("unicode", "☃");
         assertEquals("https://brightcove.com:8443/login?email=jrh%40example.net&unicode=%E2%98%83", result.unparse());
     }
 
@@ -27,7 +37,9 @@ public class SyntaxDemoTest {
     @Test
     public void testQS() throws MalformedURLException {
         Url orig = Urls.parse("http://brightcove.com/login?a=b=c&a=?d");
-        Url result = orig.withQuery(orig.getQuery().append("foo", "bar").replace("a", null));
+        Url result = orig.withQuery(orig.getQuery()
+                                        .append("foo", "bar")
+                                        .replace("a", null));
         assertEquals("http://brightcove.com/login?a&foo=bar", result.unparse());
     }
 }
