79348a7c17868318070eee7aaf71221a0804e19f
Tim McCormack <tmccormack@brightcove.com>
2015-04-29 19:22:37 -0400

    Create javadoc and sources jars too

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject org.timmc/johnny "0.1.0-SNAPSHOT"
-  :min-lein-version  "2.0.0"
+  :min-lein-version  "2.5.1" ;; for classified jars
   :description "A URL parsing and manipulation library for the JVM emphasizing standards-compliance, flexibility, and ease of use."
   :url "https://github.com/timmc/johnny"
   :dependencies [[org.clojure/clojure "1.5.1"]
@@ -14,6 +14,18 @@
                    :java-source-paths ["src/test/java"]}}
   :plugins [[lein-junit "1.1.8"]]
   :junit ["src/test/java"]
+  :omit-source true
+  :classifiers
+  {:sources {:java-source-paths ^:replace []
+             :resource-paths ["src/main/java"]}
+   :javadoc {:plugins [[lein-javadoc "0.1.1"]]
+             :resource-paths ^:replace ["target/javadoc/javadoc"]
+             :javadoc-opts {:package-names ["org.timmc.johnny"]
+                            :output-dir "target/javadoc/javadoc"
+                            :additional-args ["-Xdoclint:-missing" "-version" "-charset" "UTF-8"
+                                              "-docencoding" "UTF-8" "-encoding" "UTF-8"]}
+             ;; why? :javac-options ["-target" "1.5" "-source" "1.5"]
+             :prep-tasks ["javadoc"]}}
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"
             :distribution :repo}
