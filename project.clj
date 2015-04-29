316a27e914d0951be899616aa506fe503b669e64
Tim McCormack <tmccormack@brightcove.com>
2015-04-29 13:28:23 -0400

    Fix test source path

--- project.clj
+++ project.clj
@@ -5,12 +5,13 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.google.guava/guava "14.0.1"]]
   ;; Implementation is actually in Java
-  :java-source-paths ["src/main/java" "src/test/java"]
+  :java-source-paths ["src/main/java"]
   ;; Clojure is used for testing
   :source-paths []
   :test-paths ["src/test/clojure"]
   ;; We have a tiny bit of Java for testing
-  :profiles {:dev {:dependencies [[junit/junit "4.12"]]}}
+  :profiles {:dev {:dependencies [[junit/junit "4.12"]]
+                   :java-source-paths ["src/test/java"]}}
   :plugins [[lein-junit "1.1.8"]]
   :junit ["src/test/java"]
   :license {:name "Apache License, Version 2.0"
