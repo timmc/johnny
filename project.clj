93a9a4fbcff4528bb135164bc7eb1d362fa401b1
Tim McCormack <tmccormack@brightcove.com>
2015-05-01 16:59:45 -0400

    Use own fork of lein-javadoc

--- project.clj
+++ project.clj
@@ -12,26 +12,21 @@
   ;; We have a tiny bit of Java for testing
   :profiles {:dev {:dependencies [[junit/junit "4.12"]]
                    :java-source-paths ["src/test/java"]}}
-  :plugins [[lein-junit "1.1.8"]]
+  :plugins [[lein-junit "1.1.8"]
+            [org.clojars.brightcove/lein-javadoc "0.2.0"]]
   :junit ["src/test/java"]
   :omit-source true
   :classifiers
   {:sources
    {:java-source-paths ^:replace []
     :resource-paths ["src/main/java"]}
-   ;; FIXME: Includes with tools.jar and sa-jdi.jar in javadoc jar
-   ;; even with jar exclusions.
-   ;; https://github.com/davidsantiago/lein-javadoc/issues/1
-   ;; https://github.com/pallet/lein-jdk-tools/issues/2
    :javadoc
-   {:plugins [[lein-javadoc "0.1.1"]]
-    :resource-paths ^:replace ["target/javadoc/javadoc"]
+   {:resource-paths ^:replace ["target/javadoc/javadoc"]
     :javadoc-opts
     {:package-names ["org.timmc.johnny"]
      :output-dir "target/javadoc/javadoc"
      :additional-args ["-Xdoclint:-missing" "-version" "-charset" "UTF-8"
                        "-docencoding" "UTF-8" "-encoding" "UTF-8"]}
-    ;; why? :javac-options ["-target" "1.5" "-source" "1.5"]
     :prep-tasks ["javadoc"]}}
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"
