b551ba01f0e915f13d4d23b01467f77a8b9d0be0
Tim McCormack <tmccormack@brightcove.com>
2015-05-03 13:35:39 -0400

    Bring project into line with Maven Central requirements.
    - Generate separate -sources and -javadoc jars, omit source from main jar
      (also requires newer leiningen)
    - Deployment info for Sonatype OSS
    - Metadata such as license, SCM, developer info, URL
    
    Also:
    
    - Delete and ignore POM since we can just use leiningen now
    - Add organizational comments to project
    - Bump to 0.2.0 to reflect 0.1.1 fork release under org.timmc

--- project.clj
+++ project.clj
@@ -1,14 +1,54 @@
-(defproject com.brightcove/johnny "0.0.1-SNAPSHOT"
-  :min-lein-version  "2.0.0"
+(defproject com.brightcove/johnny "0.2.0-SNAPSHOT"
+  :min-lein-version  "2.5.1" ;; for sources and javadoc jars
+  :description "A URL parsing and manipulation library for the JVM emphasizing standards-compliance, flexibility, and ease of use."
+  :url "https://github.com/brightcove/johnny"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.google.guava/guava "14.0.1"]]
-  ;; Implementation is actually in Java
+  :plugins [[lein-junit "1.1.8"]
+            [lein-javadoc "0.2.0"]]
+  ;; Implementation is actually in Java, but most tests are written in
+  ;; Clojure. There's one junit test in Java just to demonstrate
+  ;; syntax.
   :java-source-paths ["src/main/java"]
-  ;; Clojure is used for testing
   :source-paths []
   :test-paths ["src/test/clojure"]
-  ;; We have a tiny bit of Java for testing
   :profiles {:dev {:dependencies [[junit/junit "4.12"]]
                    :java-source-paths ["src/test/java"]}}
-  :plugins [[lein-junit "1.1.8"]]
-  :junit ["src/test/java"])
+  :junit ["src/test/java"]
+  ;; Since we're deploying Java to Maven Central, we omit source from
+  ;; the main jar and produce separate -sources and -javadoc jars.
+  :omit-source true
+  :classifiers
+  {:sources
+   {:java-source-paths ^:replace []
+    :resource-paths ["src/main/java"]}
+   :javadoc
+   {:resource-paths ^:replace ["target/javadoc/javadoc"]
+    :javadoc-opts
+    {:package-names ["com.brightcove.johnny"]
+     :output-dir "target/javadoc/javadoc"
+     :additional-args ["-Xdoclint:-missing" "-version" "-charset" "UTF-8"
+                       "-docencoding" "UTF-8" "-encoding" "UTF-8"]}
+    :prep-tasks ["javadoc"]}}
+  :deploy-repositories
+  [["releases"
+    {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+     :creds :gpg}]
+   ["snapshots"
+    {:url "https://oss.sonatype.org/content/repositories/snapshots/"
+     :creds :gpg}]]
+  ;; Finally, metadata about the project: License, SCM link, developer
+  ;; info. This is required by Sonatype OSS/Maven Central as part of
+  ;; the generated POM file.
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"
+            :distribution :repo}
+  :scm {:name "git" :url "git@github.com:brightcove/johnny.git"}
+  :pom-addition
+  [:developers
+   [:developer
+    [:name "Tim McCormack"]
+    [:email "tmccormack@brightcove.com"]
+    [:url "https://www.brainonfire.net/"]
+    [:organization "Brightcove Inc."]
+    [:organizationUrl "https://www.brightcove.com/"]]])
