2aa6f518760d0c7740e6af66ef708d84b37511b3
Tim McCormack <tmccormack@brightcove.com>
2018-05-08 18:44:44 -0400

    Expand URI parser to include reference URIs

--- src/main/java/org/timmc/johnny/AntlrUriParser.java
+++ src/main/java/org/timmc/johnny/AntlrUriParser.java
@@ -6,6 +6,10 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
+import org.timmc.johnny.ImmutableUrl;
+import org.timmc.johnny.Url;
+import org.timmc.johnny.UrlDecodeException;
+import org.timmc.johnny.UrlParser;
 import org.timmc.johnny.antlr.FlaggingErrorListener;
 import org.timmc.johnny.gen.parse.RFC_3986_6874Lexer;
 import org.timmc.johnny.gen.parse.RFC_3986_6874Parser;
@@ -20,15 +24,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A strict URL parser directly derived from ABNF grammars in the relevant RFCs.
+ * A strict URI parser directly derived from ABNF grammars in the relevant RFCs.
  */
 public class AntlrUriParser implements UrlParser {
 
     public Url parse(String in) throws UrlDecodeException {
         try {
             return parseInner(in);
-        } catch(RecognitionException | ParseCancellationException e) {
-            throw new UrlDecodeException("Could not recognize URL from input", e);
+        } catch (RecognitionException | ParseCancellationException e) {
+            throw new UrlDecodeException("Could not recognize URI from input", e);
+        } catch (NullPointerException npe) {
+            throw new UrlDecodeException("Unexpected null reference when reading URI parse tree", npe);
         }
     }
 
@@ -47,76 +53,125 @@ public class AntlrUriParser implements UrlParser {
         parser.addErrorListener(errorListener);
         lexer.addErrorListener(errorListener);
 
-        UriContext uri = parser.uri();
-        SchemeContext scheme = uri.scheme();
-        Hier_partContext hier = uri.hier_part();
-        AuthorityContext authority = hier.authority();
-        UserinfoContext userinfo = authority.userinfo();
-        HostContext host = authority.host();
+        Uri_referenceContext root = parser.uri_reference();
+        Url ret = parseUri(root);
+
+        String errMsg = errorListener.getError();
+        if (errMsg != null) {
+            throw new UrlDecodeException("Could not parse URI: " + errMsg);
+        }
+
+        // Check if entire string was consumed and matched
+        String matched = root.getText();
+        if (!in.equals(matched)) {
+            throw new UrlDecodeException("Could not parse URI: " +
+                    "error at position " + matched.length());
+        }
+
+        return ret;
+    }
+
+    private Url parseUri(Uri_referenceContext root) {
+        String schemeRaw;
+        String userInfoRaw;
+        Host host;
+        String portRaw;
+        String pathRaw;
+        String queryRaw;
+        String fragmentRaw;
+
+        UriContext uriAbs = root.uri();
+        Relative_refContext uriRef = root.relative_ref();
+
+        if (uriAbs != null) {
+            schemeRaw = uriAbs.scheme().getText();
+        } else {
+            schemeRaw = null;
+        }
+
+        AuthorityContext authority;
+        if (uriAbs != null) {
+            Hier_partContext hier = uriAbs.hier_part();
+            authority = hier.authority();
+            pathRaw = firstPath(
+                    hier.path_abempty(),
+                    hier.path_absolute(),
+                    hier.path_rootless(),
+                    hier.path_empty()
+            );
+        } else {
+            Relative_partContext relpart = uriRef.relative_part();
+            authority = relpart.authority();
+            pathRaw = firstPath(
+                    relpart.path_abempty(),
+                    relpart.path_absolute(),
+                    relpart.path_noscheme(),
+                    relpart.path_empty()
+            );
+        }
+
+        if (authority != null) {
+            UserinfoContext userinfo = authority.userinfo();
+            userInfoRaw = userinfo == null ? null : userinfo.getText();
+            HostContext hostRaw = authority.host();
+            host = hostRaw == null ? null : parseHost(hostRaw);
+            PortContext port = authority.port();
+            portRaw = port == null ? null : port.getText();
+        } else {
+            userInfoRaw = null;
+            host = null;
+            portRaw = null;
+        }
+
+        QueryContext query = uriAbs != null ? uriAbs.query() : uriRef.query();
+        queryRaw = maybeText(query);
+        Fragment_1Context fragment = uriAbs != null ? uriAbs.fragment_1() : uriRef.fragment_1();
+        fragmentRaw = maybeText(fragment);
+
+        return new ImmutableUrl(schemeRaw, userInfoRaw, host, portRaw, pathRaw, queryRaw, fragmentRaw);
+    }
+
+    public Host parseHost(HostContext host) {
         Reg_nameContext regname = host.reg_name();
         Ipv4addressContext ipv4 = host.ipv4address();
         Ip_literalContext ipNewer = host.ip_literal();
         Ipv6addressContext ipv6 = ipNewer == null? null : ipNewer.ipv6address();
         Ipv6addrzContext ipv6zoned = ipNewer == null? null : ipNewer.ipv6addrz();
         IpvfutureContext ipFuture = ipNewer == null ? null : ipNewer.ipvfuture();
-        PortContext port = authority.port();
-        Path_abemptyContext path = hier.path_abempty(); // the only one that involves an authority
-        QueryContext query = uri.query();
-        Fragment_1Context fragment = uri.fragment_1();
 
-        Host parsedHost;
         if (regname != null) {
-            parsedHost = new RegNameHost(host.getText());
+            return new RegNameHost(host.getText());
         } else if (ipv4 != null) {
             List<Integer> octets = new ArrayList<>(4);
             for (Dec_octetContext octet : ipv4.dec_octet()) {
                 octets.add(Integer.parseInt(octet.getText()));
             }
-            parsedHost = new IPv4Host(octets, host.getText());
+            return new IPv4Host(octets, host.getText());
         } else if (ipv6 != null) {
-            parsedHost = new IPv6Host(ipv6.getText(), null, host.getText());
+            return new IPv6Host(ipv6.getText(), null, host.getText());
         } else if (ipv6zoned != null) {
             String ipv6addr = ipv6zoned.ipv6address().getText();
             String zone = ipv6zoned.zoneid().getText();
-            parsedHost = new IPv6Host(ipv6addr, zone, host.getText());
+            return new IPv6Host(ipv6addr, zone, host.getText());
         } else if (ipFuture != null ){
             StringBuilder hexver = new StringBuilder();
             for (HexdigContext digit: ipFuture.hexdig()) {
                 hexver.append(digit.getText());
             }
             int formatVersion = Integer.parseInt(hexver.toString(), 16);
-            parsedHost = new IPvFutureHost(formatVersion, host.getText());
+            return new IPvFutureHost(formatVersion, host.getText());
         } else {
-            throw new UrlDecodeException("Was not able to determine format of URI host");
+            throw new UrlDecodeException("Grammar mismatch: authority did not contain any known host variant");
         }
+    }
 
-        Url ret;
-        try {
-            ret = new ImmutableUrl(
-                        maybeText(scheme),
-                        maybeText(userinfo),
-                        parsedHost,
-                        maybeText(port),
-                        maybeText(path),
-                        maybeText(query),
-                        maybeText(fragment));
-        } catch(NumberFormatException nfe) {
-            throw new UrlDecodeException("Could not parse URL: Invalid port number");
+    private String firstPath(ParserRuleContext... paths) {
+        for (ParserRuleContext path: paths) {
+            if (path != null) {
+                return path.getText();
+            }
         }
-
-        String errMsg = errorListener.getError();
-        if (errMsg != null) {
-            throw new UrlDecodeException("Could not parse URL: " + errMsg);
-        }
-
-        // Check if entire string was consumed and matched
-        String matched = uri.getText();
-        if (!in.equals(matched)) {
-            throw new UrlDecodeException("Could not parse URL: " +
-                    "error at position " + matched.length());
-        }
-
-        return ret;
+        throw new UrlDecodeException("Grammar mismatch: hier_part/rel_part did not contain any known path variant");
     }
 
     private String maybeText(ParserRuleContext x) {
